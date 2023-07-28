99173dd10e79605bfa4f77b55c470102d5cd6b18
lothrazar <samson.bassett@gmail.com>
2023-07-28 09:25:28 -0700

    quick insert feature

--- src/main/java/com/lothrazar/pickybags/event/PickupClientEvents.java
+++ src/main/java/com/lothrazar/pickybags/event/PickupClientEvents.java
@@ -6,19 +6,27 @@ import com.lothrazar.pickybags.item.IPickupable;
 import com.lothrazar.pickybags.net.PacketInsertBag;
 import com.lothrazar.pickybags.net.PacketOpenBag;
 import com.lothrazar.pickybags.net.PacketRegistry;
+import com.lothrazar.pickybags.registry.ModBagsRegistry;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.gui.screens.inventory.CreativeModeInventoryScreen;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.RegisterColorHandlersEvent;
 import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
+@EventBusSubscriber(value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
 public class PickupClientEvents {
 
+  //RIGHT CLICK to open bag
   @SubscribeEvent(priority = EventPriority.HIGH) // WAS MouseClickedEvent
   public void onMouseEvent(ScreenEvent.MouseButtonPressed.Pre event) {
     if (event.getScreen() == null || !(event.getScreen() instanceof AbstractContainerScreen<?>)) {
@@ -37,6 +45,7 @@ public class PickupClientEvents {
     }
   }
 
+  //INSERT into  the container on Left click
   @SubscribeEvent
   public void onMouseButtonReleased(ScreenEvent.MouseButtonReleased.Pre event) {
     Minecraft mc = Minecraft.getInstance();
@@ -49,7 +58,7 @@ public class PickupClientEvents {
         if (stackTarget.getItem() instanceof IPickupable pug
             && mc.player != null
             && mc.player.containerMenu != null) {
-          // 
+          // never send item only slot in packet
           //
           if (pug.canInsert(mc.player.containerMenu.getCarried())) {
             int slotId = gui.getSlotUnderMouse().getContainerSlot();
@@ -61,5 +70,44 @@ public class PickupClientEvents {
         }
       }
     }
+  }
+
+  //render toggle
+  @SubscribeEvent
+  public void onScreenRender(ScreenEvent.Render.Pre event) {
+    Minecraft mc = Minecraft.getInstance();
+    Screen screen = mc.screen;
+    if (screen instanceof AbstractContainerScreen<?> gui && !(screen instanceof CreativeModeInventoryScreen)) {
+      ItemStack maybeFood = mc.player.containerMenu.getCarried();
+      Inventory playerInventory = mc.player.getInventory();
+      for (int x = 0; x < playerInventory.getContainerSize(); x++) {
+        ItemStack stack = playerInventory.getItem(x);
+        if (stack.getItem() instanceof IPickupable pu) {
+          //its edible food // it fits into the box 
+          pu.setBoxInsertable(stack, pu.canInsert(maybeFood));
+        }
+      }
+    }
+  }
+
+  @OnlyIn(Dist.CLIENT)
+  @SubscribeEvent
+  public static void registerItemColors(RegisterColorHandlersEvent.Item event) {
+    event.register((stack, tintIndex) -> {
+      if (tintIndex == 0) { //layer zero is outline, ignore this 
+        return 0xFFFFFFFF;
+      }
+      //layer 1 is overlay  
+      return getColour(stack);
+    }, ModBagsRegistry.BOX.get()); // TODO: edit textures of bags for layered insert
+  }
+
+  private static int getColour(ItemStack stack) {
+    if (stack.getItem() instanceof IPickupable &&
+        stack.hasTag() && stack.getTag().getBoolean(IPickupable.HOLDING)) {
+      // green? return 0x00AAAAFF;
+      return 0x000000FF; //  0xFFFF0011;
+    }
+    return 0xFFFFFFFF;
   }
 }
--- src/main/java/com/lothrazar/pickybags/item/IPickupable.java
+++ src/main/java/com/lothrazar/pickybags/item/IPickupable.java
@@ -4,5 +4,11 @@ import net.minecraft.world.item.ItemStack;
 
 public interface IPickupable extends IOpenable {
 
+  public static final String HOLDING = "holding";
+
   public boolean canInsert(ItemStack itemPickup);
+
+  default void setBoxInsertable(ItemStack box, boolean edible) {
+    box.getOrCreateTag().putBoolean(HOLDING, edible);
+  }
 }
